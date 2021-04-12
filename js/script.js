15f11410ff890bbb29269f60a02a5c182a9c995d
Leon <leon@famnabuurs.nl>
2021-04-12 15:06:24 +0200

    Afgerond

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// dit is een grote som
+let uitkomst = 1230941 * 1823794;
+console.log(uitkomst);
+
+uitkomst = 637263 / 54;
+console.log(uitkomst);
+
+/* dit is een groot commentaarblok
+let uitkomst = 1230941 * 1823794;
+console.log(uitkomst);
+
+uitkomst = 637263 / 54;
+console.log(uitkomst);
+
+ */
+
